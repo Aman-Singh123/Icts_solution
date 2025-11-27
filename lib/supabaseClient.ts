d268b76cffa793e9f50e64c47fe987fe37ca23a7
Aman-Singh123 <amansingh701173@gmail.com>
2025-11-27 14:32:43 +0530

    supa

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,9 @@
-
+// lib/supabaseAdmin.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY!; // server-only env
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(url, serviceRole, {
+  auth: { persistSession: false },
+});
